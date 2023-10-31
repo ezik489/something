0d1d42b4e34bd80864c6f71d4f7f98c8c6ce5708
ezik489 <patrick.safarik@gmail.com>
2023-10-31 17:38:17 +0100

    h

---
+++ letters.py
@@ -0,0 +1,21 @@
+
+from turtle import *
+def letter_H():
+    speed(1)
+    left(90)
+    forward(50)
+    left(180)
+    forward(100)
+    left(180)
+    forward(50)
+    right(90)
+    forward(40)
+    right(90)
+    forward(50)
+    left(180)
+    forward(100)
+    left(180)
+    forward(100)
+
+letter_H()
+exitonclick()
