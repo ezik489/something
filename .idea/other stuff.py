645eacb6258f39b20cdbe2eacc2d2525ea74a3b4
ezik489 <patrick.safarik@gmail.com>
2023-11-07 18:09:03 +0100

    somthing file

---
+++ .idea/other stuff.py
@@ -0,0 +1,8 @@
+word = input("write a word: ")
+
+for letter in word:
+
+    if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+        print(letter)
+
+print("these are the voules of the givven word in order")
