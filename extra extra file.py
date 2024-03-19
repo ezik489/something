bddf2b741829f0a17fa53bdcebe1fe01c24cdea0
ezik489 <patrick.safarik@gmail.com>
2024-03-19 17:25:54 +0100

    somthing file

--- extra extra file.py
+++ extra extra file.py
@@ -1,69 +1,14 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-12
-13
-14
-15
-16
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
-57
-58
-59
-60
-61
-62
-63
-64
-65
-66
-67
-68
-69
+import sys
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+screen.fill()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
